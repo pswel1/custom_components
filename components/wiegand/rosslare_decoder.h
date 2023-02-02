766ea5fb87c2a8e34ef2d78451501164b2fb2490
pswel1 <88628526+pswel1@users.noreply.github.com>
2023-02-02 08:10:37 +0000

    Update rosslare_decoder.h

--- components/wiegand/rosslare_decoder.h
+++ components/wiegand/rosslare_decoder.h
@@ -3,9 +3,9 @@ inline uint8_t get_key_from_rosslare_bits(uint64_t bits) {
     if (bits == 0b110100) {
         return 0;
     } else if (bits == 0b110111) {
-        return '*';
+        return 11;
     } else if (bits == 0b011001) {
-        return '#';
+        return 22;
     } else {
         return (bits % 0x3C) / 2 & 0xF;
     }
